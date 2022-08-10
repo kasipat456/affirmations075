71b5d5dea127c6770d3a759eda23fff598712baf
kasipat456 <63410075@go.buu.ac.th>
2022-08-10 16:56:03 +0700

    affirmations075

---
+++ app/src/main/java/com/example/affirmations075/data/Datasource.kt
@@ -0,0 +1,70 @@
+package com.example.affirmations075.data
+
+import com.example.affirmations075.R
+import com.example.affirmations075.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10),
+            Affirmation(R.string.affirmation11),
+            Affirmation(R.string.affirmation12),
+            Affirmation(R.string.affirmation13),
+            Affirmation(R.string.affirmation14),
+            Affirmation(R.string.affirmation15),
+            Affirmation(R.string.affirmation16),
+            Affirmation(R.string.affirmation17),
+            Affirmation(R.string.affirmation18),
+            Affirmation(R.string.affirmation19),
+            Affirmation(R.string.affirmation20),
+            Affirmation(R.string.affirmation21),
+            Affirmation(R.string.affirmation22),
+            Affirmation(R.string.affirmation23),
+            Affirmation(R.string.affirmation24),
+            Affirmation(R.string.affirmation25),
+            Affirmation(R.string.affirmation26),
+            Affirmation(R.string.affirmation27),
+            Affirmation(R.string.affirmation28),
+            Affirmation(R.string.affirmation29),
+            Affirmation(R.string.affirmation30),
+            Affirmation(R.string.affirmation31),
+            Affirmation(R.string.affirmation32),
+            Affirmation(R.string.affirmation33),
+            Affirmation(R.string.affirmation34),
+            Affirmation(R.string.affirmation35),
+            Affirmation(R.string.affirmation36),
+            Affirmation(R.string.affirmation37),
+            Affirmation(R.string.affirmation38),
+            Affirmation(R.string.affirmation39),
+            Affirmation(R.string.affirmation40),
+            Affirmation(R.string.affirmation41),
+            Affirmation(R.string.affirmation42),
+            Affirmation(R.string.affirmation43),
+            Affirmation(R.string.affirmation44),
+            Affirmation(R.string.affirmation45),
+            Affirmation(R.string.affirmation46),
+            Affirmation(R.string.affirmation47),
+            Affirmation(R.string.affirmation48),
+            Affirmation(R.string.affirmation49),
+            Affirmation(R.string.affirmation50),
+            Affirmation(R.string.affirmation51)
+
+
+
+
+
+
+
+
+        )
+    }
+}
